2cfbac76120418337d0e2737092a06e229c66982
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-09 23:41:40 +0100

    Require Node.js 8, add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Check if a string is a [scoped npm package name](https://docs.npmjs.com/misc/scope).
+ *
+ * @param input - The package name to check.
+ */
+export default function isScoped(input: string): boolean;
