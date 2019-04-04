2ae13b51d942a5d4f7e2c0177f08e50ca1c372cb
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-04 03:47:11 +0000

    Refactor TypeScript definition to CommonJS compatible export (#2)

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,24 @@
-/**
- * Check if a string is a [scoped npm package name](https://docs.npmjs.com/misc/scope).
- */
-export default function isScoped(input: string): boolean;
+declare const isScoped: {
+	/**
+	Check if a string is a [scoped npm package name](https://docs.npmjs.com/misc/scope).
+
+	@example
+	```
+	import isScoped = require('is-scoped');
+
+	isScoped('@sindresorhus/df');
+	//=> true
+
+	isScoped('cat-names');
+	//=> false
+	```
+	*/
+	(input: string): boolean;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function isScoped(input: string): boolean;
+	// export = isScoped;
+	default: typeof isScoped;
+};
+
+export = isScoped;
